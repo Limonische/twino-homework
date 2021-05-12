d141ad476782735ef60d4220f27100894630ded8
Limonische <gulisnikita@gmail.com>
2021-05-12 18:58:01 +0300

    Add first implementation if Question component

---
+++ components/Question.vue
@@ -0,0 +1,37 @@
+<template lang="pug">
+    .question
+        h3.question__heading {{ question.question }}
+            label(
+                v-for="(answer, index) in question.answers"
+                :key="answer + '-' + index"
+            )
+                | {{ answer }}
+                input(
+                    :value="answer"
+                    v-model="selectedAnswer"
+                    type="radio"
+                )
+</template>
+
+<script>
+export default {
+    props: {
+        question: {
+            type: Object,
+            required: true
+        }
+    },
+
+    data() {
+        return {
+            selectedAnswer: null
+        }
+    },
+
+    watch: {
+        selectedAnswer(newSelectedAnswer) {
+            this.$emit('changeSelectedAnswer', newSelectedAnswer)
+        }
+    }
+}
+</script>

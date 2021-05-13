87016716d1eebc06a5b5aba58926f7d130782aef
Limonische <gulisnikita@gmail.com>
2021-05-13 12:49:38 +0300

    Add styling to Error

--- components/Error.vue
+++ components/Error.vue
@@ -1,3 +1,12 @@
 <template lang="pug">
-    section.error Please provide answer
+    .error Please provide answer
 </template>
+
+<style lang="sass" scoped>
+.error
+    border: 2px solid var(--color-red-1000)
+    padding: 1rem
+    text-align: center
+    border-radius: 5px
+    margin: 3rem 0
+</style>

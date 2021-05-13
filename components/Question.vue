fe838d026b2863f8366f909a83bec66b6e852aab
Limonische <gulisnikita@gmail.com>
2021-05-13 12:40:56 +0300

    Add styling to Question

--- components/Question.vue
+++ components/Question.vue
@@ -1,16 +1,19 @@
 <template lang="pug">
     .question
         h3.question__heading {{ question.question }}
-            label(
+        .question__answers
+            label.question__answer(
                 v-for="(answer, index) in question.answers"
                 :key="answer + '-' + index"
             )
-                | {{ answer }}
-                input(
+                input.question__input(
                     :value="answer"
                     v-model="selectedAnswer"
                     type="radio"
                 )
+                span.question__label
+                    span.question__bullet
+                    span.question__text {{ answer }}
 </template>
 
 <script>
@@ -35,3 +38,36 @@ export default {
     }
 }
 </script>
+
+<style lang="sass" scoped>
+@use 'mixins' as *
+
+.question
+    margin: 3rem 0
+    &__heading
+        font-size: 1.5rem
+    &__answers
+        & > * + *
+            margin-top: .75em
+    &__answer
+        cursor: pointer
+        display: flex
+        height: 1.15em
+    &__label
+        display: flex
+    &__bullet
+        +transition(border)
+        height: 1em
+        width: 1em
+        border-radius: 50%
+        border: 1px solid var(--color-gray-1000)
+        margin-right: .5em
+    &__input
+        width: 0
+        height: 0
+        opacity: 0
+        &:checked
+            & + .question__label
+                .question__bullet
+                    border: 4px solid var(--color-blue-2000)
+</style>

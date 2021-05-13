23beb1fe183b0ecaba5fcc9cac7b67a225e5ceb2
Limonische <gulisnikita@gmail.com>
2021-05-13 14:31:14 +0300

    Add styling to Group component

--- components/Group.vue
+++ components/Group.vue
@@ -1,15 +1,23 @@
 <template lang="pug">
     section.group
-        h3.group__heading {{ group.title }}
-        .group__question(
-            v-for="question in groupQuestions"
-            :key="question.id"
-        )
-            h4 {{ question.question }}
-            a(
+        header.group__header
+            h3.group__heading {{ group.title }}
+            a.group__trigger(
                 href="#"
-                @click.prevent="onQuestionClick(question)"
-            ) {{ question.selectedAnswer }}
+                :class="{ 'group__trigger--active': showQuestions }"
+                @click.prevent="showQuestions = !showQuestions"
+            ) {{ showQuestions ? 'Show less' : 'Show more' }}
+        TransitionExpand
+            .group__questions(v-if="showQuestions")
+                section.group__question(
+                    v-for="question in groupQuestions"
+                    :key="question.id"
+                )
+                    h4.group__question-heading {{ question.question }}
+                    a.group__question-link(
+                        href="#"
+                        @click.prevent="onQuestionClick(question)"
+                    ) {{ question.selectedAnswer }}
 </template>
 
 <script>
@@ -20,6 +28,12 @@ export default {
         group: {
             type: Object,
             required: true
+        }
+    },
+
+    data() {
+        return {
+            showQuestions: false
         }
     },
 
@@ -42,3 +56,52 @@ export default {
     }
 }
 </script>
+
+<style lang="sass" scoped>
+@use 'mixins' as *
+
+.group
+    margin: 4rem 0
+    &__trigger
+        flex-shrink: 0
+        margin-left: 2rem
+        text-decoration: none
+        color: var(--color-blue-2000)
+        position: relative
+        padding-left: 1.3em
+        &::before, &::after
+            content: ''
+            display: block
+            height: 2px
+            width: 1em
+            background-color: var(--color-blue-2000)
+            position: absolute
+            left: 0
+            top: calc(50% - .075em)
+        &::after
+            +transition(transform, .5s)
+            transform: rotate(90deg)
+        &--active
+            &::after
+                transform: rotate(0deg)
+    &__header
+        display: flex
+        justify-content: space-between
+        align-items: flex-start
+        padding-bottom: 1rem
+    &__heading
+        margin: 0 0 1.5em
+    &__questions
+        & > * + *
+            margin-top: 2.5rem
+    &__question-heading
+        font-weight: normal
+        margin: 0 0 1.5em
+    &__question-link
+        +transition(filter)
+        color: var(--color-gray-1000)
+        text-decoration: none
+        font-weight: 300
+        &:hover
+            filter: brightness(.8)
+</style>
--- components/Summary.vue
+++ components/Summary.vue
@@ -1,5 +1,7 @@
 <template lang="pug">
     section.summary
+        h2.summary__heading Answers overview
+        h4.summary__subheading here you can see and change your answers
         Group(
             v-for="group in groups"
             :key="group.id"
@@ -16,3 +18,10 @@ export default {
     }
 }
 </script>
+
+<style lang="sass" scoped>
+.summary
+    &__subheading
+        font-weight: normal
+        font-size: .75rem
+</style>
